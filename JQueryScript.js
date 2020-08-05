2d1aff032ad6e17b4921b01a207e32c74b5f528a
Oluwagbemiga David <39445905+david-oluwagbemiga@users.noreply.github.com>
2020-08-05 13:28:05 +0100

    Add files via upload

---
+++ JQueryScript.js
@@ -0,0 +1,19 @@
+$.getJSON('https://api.unsplash.com/users/riri_cc/likes/?client_id=wStDWhGUBB9M3984KWuu0YW00n4eJ33zYidRsQmUGpE', function(data){
+console.log(data);
+
+$.each(data, function(index, value) {
+    console.log(value);
+
+    var name = value.user.name;
+    var bio =  value.user.bio;
+    var imageURL = value.urls.regular;
+    
+    // $('.name').text(name);
+    // $('.bio').text(bio);
+    // $('.image img').attr('src', imageURL);
+
+    $('.image').append('<div class="image"><img src="' + imageURL + '" alt=""></div>');
+
+});
+
+});
